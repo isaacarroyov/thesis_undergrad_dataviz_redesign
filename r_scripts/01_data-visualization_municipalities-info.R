53b880163d10f6b964120177fa8ca6f98dcf9e6a
Isaac Arroyo <isaacarroyov@outlook.com>
2022-06-02 21:55:00 -0500

    [UPDATE] R Script - 01 - DataViz - Municipalities Info

--- r_scripts/01_data-visualization_municipalities-info.R
+++ r_scripts/01_data-visualization_municipalities-info.R
@@ -22,32 +22,34 @@ theme_update(
   plot.background = element_rect(fill='white', color = 'white'),
   panel.background = element_rect(fill='white', color = 'white'),
   axis.title = element_blank(),
-  axis.text = element_text(face='bold', color = 'black', size = 9),
-  axis.text.x = element_text(margin = margin(20,0,0,0)),
+  axis.text = element_text(face='bold', color = 'black', size = 10),
+  axis.text.x = element_text(margin = margin(25,0,0,0)),
+  axis.text.y = element_text(margin = margin(0,30,0,0)),
   panel.grid.major = element_line(linetype = "dotted", color = 'gray90'),
   panel.grid.minor = element_blank(),
   plot.title.position = "plot",
   plot.caption.position = "plot",
-  plot.title = element_textbox_simple(size = 20, face = 'bold', margin = margin(5,0,5,0), halign = 0),
-  plot.subtitle = element_textbox_simple(size = 12, margin = margin(0,0,25,0)),
-  plot.caption = element_textbox_simple(size = 7, halign = 0, margin = margin(13,0,0,0)),
+  plot.title = element_textbox_simple(size = 18, face = 'bold', margin = margin(5,0,5,0), halign = 0.5),
+  plot.subtitle = element_textbox_simple(size = 10, margin = margin(0,0,25,0)),
+  plot.caption = element_textbox_simple(size = 9, halign = 0, margin = margin(10,0,0,0)),
 )
 
 # ENGLISH - SCATTER PLOT
+(
 p1 <- df %>% filter(cluster != 0) %>%
   ggplot(aes(x=freq, y=affected_area)) +
   geom_mark_ellipse(aes(fill=cluster), color = "transparent", alpha = 0.75) + 
-  geom_point(color='black', size = 3.5) +
+  geom_point(color='black', size = 3) +
   geom_text_repel(aes(label = name, family = "Optima", fontface = 'bold.italic'),
-                  force = 30, size = 3.5,
+                  force = 1, size = 3.5,
                   color = 'black',seed = 11) + 
   annotate(geom = "text", x = 12, y = 1500,
            size = 3.45, family = 'Optima', fontface = 'italic',
            label = "1653 ha affected\nby 9 wildfires") +
-  annotate(geom = "text", x = 3, y = 1400,
+  annotate(geom = "text", x = 3.2, y = 1400,
            size = 3.45, family = 'Optima', fontface = 'italic',
            label = "1304 ha affected\nby 1 wildfire") +
-  annotate(geom = "text", x = 12, y = 1000,
+  annotate(geom = "text", x = 12, y = 1050,
            size = 3.45, family = 'Optima', fontface = 'italic',
            label = "630 ha affected\nby 13 wildfires") +
   geom_curve(data = tribble(
@@ -63,17 +65,19 @@ p1 <- df %>% filter(cluster != 0) %>%
     linetype = 'dashed') + 
   scale_fill_manual(values = met.brewer("Hiroshige",3)) +
   scale_y_continuous(breaks = seq(0,1600,400), labels = paste(seq(0,1600,400),"ha")) + 
-  scale_x_continuous(breaks = seq(0,13,2), labels = paste(seq(0,13,2), "wildfires")) + 
+  scale_x_continuous(breaks = seq(0,13,3), labels = paste(seq(0,13,3), "wildfires"), expand = expansion(mult = 0.15)) + 
   coord_cartesian(clip = "off") +
   labs(
     title='Municipalities affected by wildfires during 2017<br>',
     subtitle = "The visualization showcases the relationship between the <b>number of wildfires</b> (_x-axis_) and <b>affected area</b> (in **hectares** or **ha**) caused by them (_y-axis_). Besides, the data is grouped in three different clusters*.",
-    caption = "*<span style='color:#DF9901'><b>Group 2</b></span> has the municipalities with the largest affected areas. The municipalities of <span style='color:#EA701F'><b>Group 1</b></span> and <span style='color:#44A7C5'><b>Group 3</b></span> have lower affected areas, but they differ in the number of records of wildfires.<br><b>Source:</b> _Comisión Nacional Forestal (CONAFOR), 2017_.<br><b>Visualization by Isaac Arroyo (@unisaacarroyov on Twitter and Behance)</b>"
-       ) + theme(plot.subtitle = element_textbox_simple(margin = margin(-20,0,20,0)))
-p1
+    caption = "*<span style='color:#DF9901'><b>Group 2</b></span> has the municipalities with the largest affected areas. The municipalities of <span style='color:#EA701F'><b>Group 1</b></span> and <span style='color:#44A7C5'><b>Group 3</b></span> have lower affected areas, but they differ in the number of records of wildfires.<br><b>Source:</b> Comisión Nacional Forestal (CONAFOR), 2017.<br><b>Visualization by Isaac Arroyo (@unisaacarroyov on Twitter and Behance)</b>",
+    #caption = "<b>Source:</b> Comisión Nacional Forestal (CONAFOR), 2017.<br><b>Visualization by Isaac Arroyo (@unisaacarroyov on Twitter and Behance)</b>"
+    ) + theme(plot.subtitle = element_textbox_simple(margin = margin(-20,0,20,0)))
+)
 ggsave("./images/01_municipalities-info_scatterplot.png", units = "px", height = 675, width = 540, scale = 3)
 
 # ENGLISH - MAP
+(
 p2 <- df %>% filter(cluster != 0) %>%
   ggplot(aes(fill=cluster)) +
   geom_sf(data = df %>% filter(cluster==0), color = 'white', fill = 'gray90', size=0.1) +
@@ -85,15 +89,12 @@ p2 <- df %>% filter(cluster != 0) %>%
   labs(
     title='Map of municipalities affected by wildfires during 2017',
     subtitle = "<br><br><span style='color:#DF9901'><b>Group 2</b></span> has the municipalities with the largest affected areas. The municipalities of <span style='color:#EA701F'><b>Group 1</b></span> and <span style='color:#44A7C5'><b>Group 3</b></span> have lower affected areas, but they differ in the number of records of wildfires.",
-    caption = "**Source:** _Comisión Nacional Forestal (CONAFOR), 2017_.<br>**Visualization by Isaac Arroyo (@unisaacarroyov on Twitter and Behance)**"
+    caption = "**Source:** Comisión Nacional Forestal (CONAFOR), 2017.<br>**Visualization by Isaac Arroyo (@unisaacarroyov on Twitter and Behance)**"
   ) +
   coord_sf(xlim = c(3700663, 3994857),
-           ylim = c(897964.9, 1140000)) + 
-  theme(
-    plot.subtitle = element_textbox_simple(margin = margin(-20,0,20,0)),
-  )
-p2
+           ylim = c(897964.9, 1140000))
+)
 ggsave("./images/01_municipalities-info_map.png", units = "px", height = 675, width = 540, scale = 3)
 
-(p1/p2) + plot_layout(heights = c(0.5,0.5))
-ggsave("./images/01_municipalities-info.png", units = "px", height = 1350, width = 540, scale = 3)
+(p1+p2) + plot_layout(widths = c(0.5,0.5))
+ggsave("./images/01_municipalities-info.png", units = "px", width = 1080, height = 675, scale = 3)
